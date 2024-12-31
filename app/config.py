351013793f15c83e2c1a250d76b0f4041b81c6eb
theSchein <benscheinberg@gmail.com>
2024-12-31 14:07:10 +0000

    added approval contract interface

--- app/config.py
+++ app/config.py
@@ -28,6 +28,36 @@ USDC_ABI = [
     {"inputs": [{"name": "to", "type": "address"}, {"name": "amount", "type": "uint256"}], "name": "transfer", "outputs": [{"name": "", "type": "bool"}], "stateMutability": "nonpayable", "type": "function"}
 ]
 
+# src/config.py
+import os
+from dotenv import load_dotenv
+import logging
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+PRIVATE_KEY = os.getenv("POLYGON_WALLET_PRIVATE_KEY")
+if not PRIVATE_KEY:
+    raise ValueError("POLYGON_WALLET_PRIVATE_KEY not set in environment")
+
+# Contract addresses
+USDC_ADDRESS = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
+CTF_ADDRESS = "0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"
+EXCHANGE_ADDRESS = "0x4bfb41d5B3570defd03c39a9A4d8de6bd8b8982e"
+POLYGON_RPC = "https://polygon-rpc.com"
+GAMMA_URL = "https://gamma-api.polymarket.com"
+GAMMA_MARKETS_ENDPOINT = f"{GAMMA_URL}/markets"
+SUBGRAPH_URL = 'https://api.goldsky.com/api/public/project_cl6mb8i9h0003e201j6li0diw/subgraphs/positions-subgraph/0.0.7/gn'
+
+# Contract ABIs
+USDC_ABI = [
+    {"inputs": [{"name": "account", "type": "address"}], "name": "balanceOf", "outputs": [{"name": "", "type": "uint256"}], "stateMutability": "view", "type": "function"},
+    {"inputs": [{"name": "owner", "type": "address"}, {"name": "spender", "type": "address"}], "name": "allowance", "outputs": [{"name": "", "type": "uint256"}], "stateMutability": "view", "type": "function"},
+    {"inputs": [{"name": "spender", "type": "address"}, {"name": "amount", "type": "uint256"}], "name": "approve", "outputs": [{"name": "", "type": "bool"}], "stateMutability": "nonpayable", "type": "function"},
+    {"inputs": [{"name": "to", "type": "address"}, {"name": "amount", "type": "uint256"}], "name": "transfer", "outputs": [{"name": "", "type": "bool"}], "stateMutability": "nonpayable", "type": "function"}
+]
+
 CTF_ABI = [
     {
         "constant": True,
@@ -67,6 +97,26 @@ CTF_ABI = [
         "name": "redeemPositions",
         "outputs": [],
         "payable": False,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {"name": "account", "type": "address"},
+            {"name": "operator", "type": "address"}
+        ],
+        "name": "isApprovedForAll",
+        "outputs": [{"name": "", "type": "bool"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {"name": "operator", "type": "address"},
+            {"name": "approved", "type": "bool"}
+        ],
+        "name": "setApprovalForAll",
+        "outputs": [],
         "stateMutability": "nonpayable",
         "type": "function"
     }
